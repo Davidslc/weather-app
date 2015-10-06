4781b63a589945df1677c7999acf167003165d16
Davidslc <djjohnson@gmail.com>
2015-10-06 17:35:30 -0600

    Added date converter and formatting

--- app.js
+++ app.js
@@ -35,4 +35,8 @@ weatherApp.controller('forcastController', ['$scope', '$resource', 'cityService'
     $scope.convertToFahrenheit = function(degk) {
         return Math.round((1.8 * (degk - 273)) + 32);
     }
+    
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
 }]);
